6e46fce62efb464acf50412d599dd9c9efe86c06
nevets615 <surj316@yahoo.com>
2019-04-30 17:14:46 -0400

    i have done both gets and post

--- index.js
+++ index.js
@@ -1,10 +1,67 @@
-const express = require('express');
-const helmet = require('helmet');
+
+const knex = require("knex");
+const express = require("express");
+const helmet = require("helmet");
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+const knexConfig = {
+  client: "sqlite3",
+  connection: {
+    filename: "./data/zoos.db3"
+  }
+};
+const db = knex(knexConfig);
+
+server.get("/zoos", (req, res) => {
+  db("zoos")
+    .then(roles => {
+      res.status(200).json(roles);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+server.get("/zoos:id", (req, res) => {
+  db("zoos")
+    .where({ id: req.params.id })
+    .first()
+    .then(zoo => {
+      if (zoo) {
+        res.status(200).json(role);
+      } else {
+        res.status(404).json({ message: "Zoo not found" });
+      }
+    })
+
+    .catch(res => {
+      console.log(err);
+    });
+});
+
+server.post("/zoos", (req, res) => {
+  db("zoos")
+    .insert(req.body, "id")
+    .then(result => {
+      res.status(200).json(results);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.put("/zoos:id", (req, res) => {
+  // update roles
+  res.send("Write code to modify a role");
+});
+
+server.delete("/zoos:id", (req, res) => {
+  // remove roles (inactivate the role)
+  res.send("Write code to remove a role");
+});
 
 // endpoints here
 
